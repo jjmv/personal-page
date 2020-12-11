4950e2651e209e4a0c96654aad80f5b8d14ea6a8
Javier Moreno <jjmv.97@hotmail.com>
2020-12-11 15:29:42 -0600

    Introduction Panel, missing css typing animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,35 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
 import Projects from "./Pages/Projects/Projects";
 import Skills from "./Pages/Skills/Skills";
 import NavBar from "./Components/NavBar/NavBar";
+import "./App.scss";
 
 function App() {
   return (
     <>
       <Router>
-        <NavBar />
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/projects">
-            <Projects />
-          </Route>
-          <Route path="/skills">
-            <Skills />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+        <header>
+          <NavBar />
+        </header>
+        <main>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/projects">
+              <Projects />
+            </Route>
+            <Route path="/skills">
+              <Skills />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </main>
       </Router>
     </>
   );
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
-
+import React from "react";
+import "./_styles.scss";
 
 export default function Home() {
-
-    return(
-        <>
-       <div>Welcome Home 2222</div>
-        </>
-    )
+  return (
+    <>
+      <section className="home-section">
+        <div className="card">
+          <div className="container">
+            <div className="margin-top line-1">
+              <p className="constWord">const</p>
+              <p className="introduction-word"> introduction</p>
+              <p className="equal-symbol"> = </p>
+              <p className="parenthesis-symbol"> ()</p>
+              <p className="arrow-symbol">{" =>"}</p>
+              <p className="curlyBraces-symbol">{" {"}</p>
+            </div>
+            <div className="margin-top line-2">
+              <p className="console-word">console</p>
+              <p>.</p>
+              <p className="log-word">log</p>
+              <p className="parenthesis-symbol">(</p>
+              <p className="text">
+                {` `}"Hello! my name is Javier and I am a web developer"
+              </p>
+              <p className="parenthesis-symbol">{` `})</p>
+            </div>
+            <div className="margin-top line-3">
+              <p className="curlyBraces-symbol">{"}"}</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
